61d374ed23a8f404efed1f155303747f89467f47
tomato1125 <naokiyoshida1125@icoud.com>
2020-11-19 18:17:41 +0900

    [new]clock-class-create

---
+++ main.js
@@ -0,0 +1,24 @@
+'user strict'
+
+{
+  class Clock {
+    drawFace() {
+      const canvas = document.querySelector('canvas');
+      if (typeof canvas.getContext === 'undefined') {
+        return;
+      }
+      const ctx = canvas.getContext('2d');
+
+      const width = canvas.width;
+      const height = canvas.height;
+    }
+
+    run() {
+      this.drawFace();
+    }
+
+  }
+
+  const clock = new Clock();
+  clock.run();
+}
